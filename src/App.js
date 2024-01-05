84fe296d28bd7b7fcc4b87fdae97c04cc6248b8c
Bojan Beran <turboki@gmail.com>
2024-01-04 19:39:46 -0800

    made buttons work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [moves, setMoves] = useState([
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ]);
+  const [player, setPlayer] = useState("X");
+  const [winner, setWinner] = useState("");
+  
+  const checkIfWin = () => {
+    // Vertical
+    if (
+      (!!moves[0][0] && moves[0][0] === moves[1][0] && moves[0][0] === moves[2][0]) ||
+      (!!moves[0][1] && moves[0][1] === moves[1][1] && moves[0][1] === moves[2][1]) ||
+      (!!moves[0][2] &&moves[0][2] === moves[1][2] && moves[0][2] === moves[2][2]) 
+    ) {
+      setWinner(player);
+    }
+    // Horizontal
+
+    // Diagonal
+
+  }
+
+
+  const playMove = (row, col) => {
+    const newMoves = [...moves];
+    newMoves[row][col] = player;
+    setMoves(newMoves);
+    checkIfWin();
+    if (player === "X") {
+      setPlayer("O");
+    } else {
+      setPlayer("X");
+    }
+  }
   return (
     <div className='App'>
+      {winner && <h2>Winner {winner}</h2>}
       <div className='TicTacToeBoard'>
-        <button className='Square'>X</button>
-        <button className='Square'>O</button>
-        <button className='Square'>X</button>
-        <button className='Square'>O</button>
-        <button className='Square'>X</button>
-        <button className='Square'>O</button>
-        <button className='Square'>X</button>
-        <button className='Square'>O</button>
-        <button className='Square'>X</button>
+        {moves.map((row, row_index) => {
+          return row.map((move, col_index) => {
+           return <button className='Square' onClick={() => {playMove(row_index, col_index)}} disabled={!!move || !!winner}>{move}</button>
+          });
+        })}
       </div>
     </div>
   );
